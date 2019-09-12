3a815b1793fa316be54821531b721be0ad80bad2
ebpe <ebelmonte82@gmail.com>
2019-09-12 14:48:38 +0200

    404 styles

--- wp-content/themes/wp-bmaker/404.php
+++ wp-content/themes/wp-bmaker/404.php
@@ -1,23 +1,23 @@
 <?php get_header(); ?>
 <main>
 	<div class="container">
-		<div class="row">
-			<div class="col-md-8">
-				<!-- section -->
-				<section>
-					<!-- article -->
-					<article id="post-404">
-						<h1 class="page-header"><?php _e( 'Page not found', 'wpbmaker' ); ?></h1>
-						<h2>
-							<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'wpbmaker' ); ?></a>
-						</h2>
-					</article>
-					<!-- /article -->
-				</section>
-				<!-- /section -->
-			</div><!-- /.col-md-8 -->
-			<?php get_sidebar(); ?>
-		</div><!-- /.row -->
-	</div><!-- /.container -->
+        <!-- section -->
+        <section>
+            <!-- article -->
+            <article id="post-404" class="p-5">
+                <div class="error-title d-flex flex-column flex-md-row align-items-center justify-content-center p-md-5 ">
+                    <img class="img-fluid" src="<?php bloginfo('template_url'); ?>/img/404.svg">
+                    <div class="error-cont pl-5">
+                        <h1><?php _e( '404', 'wpbmaker' ); ?></h1>
+                        <p><?php _e( '¡Vaya! La página a la que intentas acceder no existe.', 'wpbmaker' ); ?></p>
+                        <a class="font-weight-bold" href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'wpbmaker' ); ?></a>
+                    </div>
+                </div>
+            </article>
+            <!-- /article -->
+        </section>
+        <!-- /section -->
+    </div><!-- /.container -->
+    <?php get_sidebar(); ?>
 </main>
 <?php get_footer(); ?>
